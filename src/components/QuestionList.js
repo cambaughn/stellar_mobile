1fc5015c58df548b68d9851fb4e1c1364277f154
Cameron Baughn <cam.baughn@gmail.com>
2017-06-10 14:57:19 -0700

    Add basic style to UnansweredQuestion component

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, ScrollView } from 'react-native';
 
 import UnansweredQuestion from './UnansweredQuestion';
 
 
 const QuestionList = ({ questions }) => {
   return (
-    <View>
+    <ScrollView style={styles.container}>
       { questions.map(question => {
         return <UnansweredQuestion key={question.id} question={question} />
       })}
-    </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingTop: 50,
+
+    backgroundColor: 'white',
+
+  }
+})
+
 
 export default QuestionList;
--- src/components/UnansweredQuestion.js
+++ src/components/UnansweredQuestion.js
@@ -5,11 +5,30 @@ import { NativeRouter, Route, Link, Redirect } from 'react-router-native';
 
 const UnansweredQuestion = ({ question }) => {
   return (
-    <View>
-      <Text>{question.asker.name} asks {question.answerer.name}:</Text>
+    <View style={styles.container}>
+      <Text style={styles.bold}>{question.answerer.name}</Text>
       <Text>{question.text}</Text>
+      <Text>- {question.asker.name}</Text>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 50,
+
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 30,
+
+    borderBottomWidth: 1,
+    borderColor: '#ecf0f1',
+  },
+
+  bold: {
+    fontWeight: 'bold',
+  },
+})
 
 export default UnansweredQuestion;
