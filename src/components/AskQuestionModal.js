9c199d3cbf532b0407dfcae7eb6cf392f8abec7b
Cameron Baughn <cam.baughn@gmail.com>
2017-06-15 16:47:17 -0700

    Style question modal

--- src/components/AskQuestionModal.js
+++ src/components/AskQuestionModal.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Modal, TouchableHighlight } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, Modal, TouchableHighlight, TextInput } from 'react-native';
 import { Link } from 'react-router-native';
 
 import { postQuestion } from '../util/postQuestion';
+import colors from '../util/colors';
 
 const AskQuestionModal = () => {
   return (
@@ -14,12 +15,21 @@ const AskQuestionModal = () => {
       >
         <View style={styles.container}>
           <View>
-            <Text>Hello World!</Text>
+            <TextInput
+              multiline={true}
+              numberOfLines={4}
+              style={styles.input}
+              placeholder={"Ask a question"}
+              placeholderTextColor={colors.midGrey}
+            />
 
             <TouchableHighlight onPress={() => {
               console.log('sending question')
-            }}>
-              <Text>Ask</Text>
+            }}
+              style={styles.buttonPrimary}
+              underlayColor={colors.primary}
+            >
+              <Text style={styles.buttonPrimaryText}>Ask</Text>
             </TouchableHighlight>
 
           </View>
@@ -33,7 +43,44 @@ const AskQuestionModal = () => {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
+    // marginTop: 50,
+    height: '90%',
+    width: '100%',
+
+    backgroundColor: 'white',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  input: {
+    // backgroundColor: 'lightgreen',
+    width: Dimensions.get("window").width - 100,
+    minHeight: 30,
+    maxHeight: 300,
+
+    paddingLeft: 10,
+    paddingRight: 10,
+
+    fontSize: 20,
+  },
+
+  buttonPrimary: {
+    backgroundColor: colors.primary,
+
+    width: Dimensions.get("window").width - 100,
+    height: 30,
+
+    marginTop: 20,
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  buttonPrimaryText: {
+    color: 'white',
   },
 })
 
